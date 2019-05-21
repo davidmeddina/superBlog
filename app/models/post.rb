08b147c5634c49af9cb2702b95442adb25ab044c
david medina <davidmeddina@gmail.com>
2019-05-21 18:32:49 -0500

    Comment controller created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :content, length: {minimum: 5}
+  
 end
